e09811ad2b0dccae3fb26c4e3ec4f3c2a72b21b0
RayRidz <nourasaodi597@gmail.com>
2020-10-15 06:20:10 -0500

    t

---
+++ database/migrations/2020_10_15_095305_create_announcements_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('describtion');
+            $table->time('from');
+            $table->time('to');
+            $table->string('img');
+            $table->integer('doctor_id')->unsigned()->nullable();
+
+            $table->foreign('doctor_id')->references('id')
+                ->on('doctors')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
